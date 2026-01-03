51b76c0598fe80c45ec3fe027a20ae35e0981a98
Johannes <johannes.programming@gmail.com>
2026-01-03 20:34:53 +0100

    Version 0.1.0.dev0

--- src/cmp3/__init__.py
+++ src/cmp3/__init__.py
@@ -1,5 +1,2 @@
 from cmp3.core import *
 from cmp3.tests import *
-
-if __name__ == "__main__":
-    main()
--- src/cmp3/core/__init__.py
+++ src/cmp3/core/__init__.py
@@ -1,8 +1,56 @@
+from functools import partial
 from typing import *
 
-__all__ = ["main"]
+import setdoc
+
+__all__ = ["Comparable", "comparable"]
 
 
-def main(args: Optional[Iterable] = None) -> None:
-    "This function prints 'Hello World!'."
-    print("Hello World!")
+def comparable(*, overwrite: Any = False) -> type:
+    return partial(update, overwrite=overwrite)
+
+
+def update(cls: type, /, *, overwrite: Any = False) -> type:
+    @setdoc.basic
+    def __eq__(self: Self, other: Any) -> bool:
+        return self.__cmp__(other) == 0
+
+    @setdoc.basic
+    def __ge__(self: Self, other: Any) -> bool:
+        return self.__cmp__(other) >= 0
+
+    @setdoc.basic
+    def __gt__(self: Self, other: Any) -> bool:
+        return self.__cmp__(other) > 0
+
+    @setdoc.basic
+    def __le__(self: Self, other: Any) -> bool:
+        return self.__cmp__(other) <= 0
+
+    @setdoc.basic
+    def __lt__(self: Self, other: Any) -> bool:
+        return self.__cmp__(other) < 0
+
+    @setdoc.basic
+    def __ne__(self: Self, other: Any) -> bool:
+        return self.__cmp__(other) != 0
+
+    func: Callable
+    funcs: list[Callable]
+    funcs = [
+        __eq__,
+        __ge__,
+        __gt__,
+        __le__,
+        __lt__,
+        __ne__,
+    ]
+    for func in funcs:
+        if overwrite or not hasattr(cls, func.__name__):
+            setattr(cls, func.__name__, func)
+    return cls
+
+
+@comparable()
+class Comparable:
+    __slots__ = ()
